2298176af9a1e4f09720d64210e441e67863bd2a
ranjan-fullstack <ranjansahusi84@gmail.com>
2026-01-27 00:04:29 +0530

    aws automation

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 def hello_world(request):
-    return JsonResponse({"message": "Hello World from Django Backend working fine!"})
+    return JsonResponse({"message": "Sucessfully Auto Deployment done by CI CD with AWS!"})
 
     
 
