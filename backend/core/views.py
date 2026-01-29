d7ba4843f6427a1070383459a2ad7b5423fd13d1
ranjan-fullstack <ranjansahusi84@gmail.com>
2026-01-29 23:48:01 +0530

    added token

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 def hello_world(request):
-    return JsonResponse({"message": "Sucessfully Auto Deployment done by CI CD with AWS working fine!"})
+    return JsonResponse({"message": "Ranjan devops master!"})
 
     
 
